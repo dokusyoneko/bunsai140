e33260a436dfa45fcc1576acb8cd4e7e0342d20c
seita ryutaro <dokusyoneko@outlook.jp>
2026-02-01 12:23:12 +0900

    gitignore編集

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>140字小説～文彩～</title>
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+</head>
+<body>
+    <header>
+        <div class="header__logo">ヘッダーロゴ</div>
+    </header>
+    <main>
+        ここがメインです。
+    </main>
+</body>
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NovelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/novel', [NovelController::class, 'index']);
